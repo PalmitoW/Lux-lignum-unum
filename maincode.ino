cc6eb12fac671f400d0f27b222463fe55afae3f8
PalmitoW <32864738+PalmitoW@users.noreply.github.com>
2017-10-17 14:18:41 +0200

    Update maincode.ino

--- maincode.ino
+++ maincode.ino
@@ -226,3 +226,59 @@ void RGBLoop(){
     }
   }
 }
+
+
+// strobe
+void Strobe(byte red, byte green, byte blue, int StrobeCount, int FlashDelay, int EndPause){
+  for(int j = 0; j < StrobeCount; j++) {
+    setAll(red,green,blue);
+    showStrip();
+    delay(FlashDelay);
+    setAll(0,0,0);
+    showStrip();
+    delay(FlashDelay);
+  }
+ 
+ delay(EndPause);
+}
+
+// Halloween Eyes
+void HalloweenEyes(byte red, byte green, byte blue, 
+                   int EyeWidth, int EyeSpace, 
+                   boolean Fade, int Steps, int FadeDelay,
+                   int EndPause){
+  randomSeed(analogRead(0));
+  
+  int i;
+  int StartPoint  = random( 0, NUM_LEDS - (2*EyeWidth) - EyeSpace );
+  int Start2ndEye = StartPoint + EyeWidth + EyeSpace;
+  
+  for(i = 0; i < EyeWidth; i++) {
+    setPixel(StartPoint + i, red, green, blue);
+    setPixel(Start2ndEye + i, red, green, blue);
+  }
+  
+  showStrip();
+  
+  if(Fade==true) {
+    float r, g, b;
+  
+    for(int j = Steps; j >= 0; j--) {
+      r = j*(red/Steps);
+      g = j*(green/Steps);
+      b = j*(blue/Steps);
+      
+      for(i = 0; i < EyeWidth; i++) {
+        setPixel(StartPoint + i, r, g, b);
+        setPixel(Start2ndEye + i, r, g, b);
+      }
+      
+      showStrip();
+      delay(FadeDelay);
+    }
+  }
+  
+  setAll(0,0,0); // Set all black
+  
+  delay(EndPause);
+}
