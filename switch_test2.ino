a5c31a5fc65509a7d8b5a620270a26af7fce93cc
PalmitoW <32864738+PalmitoW@users.noreply.github.com>
2017-10-30 14:41:11 +0100

    Update switch_test2.ino

--- switch_test2.ino
+++ switch_test2.ino
@@ -201,23 +201,6 @@ void colorWipe(uint32_t c, uint8_t wait) {
 }
 /**************************** FONCTION PARAMETRES **********************/
 
-// Parametres fonction de base
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
-
 void showStrip() {
  #ifdef ADAFRUIT_NEOPIXEL_H 
    // NeoPixel
