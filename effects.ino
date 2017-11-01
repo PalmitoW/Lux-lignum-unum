104fbc4d8f5229b158a4edd3a5a97b67795f55f4
PalmitoW <32864738+PalmitoW@users.noreply.github.com>
2017-11-01 09:15:48 +0100

    Update effects.ino

--- effects.ino
+++ effects.ino
@@ -304,3 +304,36 @@ lueure blanche en haut du bâton?
 void laMortPeutAttendre(uint8_t wait){
 
 }
+
+/**************************** FONCTION PARAMETRES **********************/
+
+void showStrip() {
+ #ifdef ADAFRUIT_NEOPIXEL_H 
+   // NeoPixel
+   strip.show();
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H
+   // FastLED
+   FastLED.show();
+ #endif
+}
+
+void setPixel(int Pixel, byte red, byte green, byte blue) {
+ #ifdef ADAFRUIT_NEOPIXEL_H 
+   // NeoPixel
+   strip.setPixelColor(Pixel, strip.Color(red, green, blue));
+ #endif
+ #ifndef ADAFRUIT_NEOPIXEL_H 
+   // FastLED
+   leds[Pixel].r = red;
+   leds[Pixel].g = green;
+   leds[Pixel].b = blue;
+ #endif
+}
+
+void setAll(byte red, byte green, byte blue) {
+  for(int i = 0; i < NUM_LEDS; i++ ) {
+    setPixel(i, red, green, blue); 
+  }
+  showStrip();
+}
