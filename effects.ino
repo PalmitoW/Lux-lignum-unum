a6d727911ad18d273b884f3fa595779fa29bb037
PalmitoW <32864738+PalmitoW@users.noreply.github.com>
2017-11-01 09:17:48 +0100

    Update effects.ino

--- effects.ino
+++ effects.ino
@@ -304,7 +304,16 @@ lueure blanche en haut du bâton?
 void laMortPeutAttendre(uint8_t wait){
 
 }
+/*************************** BOUCLE EFFETS DE BASE ***************************/
 
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
 /**************************** FONCTION PARAMETRES **********************/
 
 void showStrip() {
