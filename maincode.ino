57e5c9502c99a020f27e06ce8c9028c3c52f4c84
PalmitoW <32864738+PalmitoW@users.noreply.github.com>
2017-11-02 14:24:45 +0100

    Update maincode.ino

--- maincode.ino
+++ maincode.ino
@@ -272,7 +272,202 @@ void traitCouleur(uint32_t c, uint8_t istart, uint8_t istop){
 		delay(50);
 	}
 }
-	
+/*
+Effet 1
+Affichage en led: 000
+Nom du sort : Dague d’Ombre
+Effet du sort : Permet d'infliger 2 points de dégâts au toucher à main nu, les dégâts traverse l'armure.
+Vous devez avoir la main nu, ne pas porter de gant.
+Ce sort ne peut être utilisé en combat, ne vous jetez pas sur un adversaire pour le toucher de votre main.
+Description effet : 
+Zone de violet tourne autour des interrupteurs
+fonction d’allumage de leds défini, augmenter l'intensite rapidement et diminution doucement
+*/
+void dagueDombre(uint8_t led1, uint8_t led2, uint8_t wait){
+    for(uint8_t j=0; j<255 ; j++){
+        for(uint8_t i=led1; i<led2; i++) {
+          strip.setPixelColor(i, j, 0, j);
+        }
+    strip.show();
+    delay(wait);
+    }
+    for(uint8_t j=255; j>0 ; j--){
+        for(uint8_t i=led1; i<led2; i++) {
+        strip.setPixelColor(i, j, 0, j);
+        }
+    strip.show();
+    delay(wait*4);
+    }
+}
+
+/*
+Effet 2
+Affichage en led: 000
+Nom du sort : Visage de cauchemar
+Effet du sort :
+Lors du lancement de ce sort, désignez -dans un cercle de 3 mètres de rayon autour de vous- autant de personnes que vous le souhaitez. Aux yeux de ces cibles, votre visage disparaît sous un masque d'ombre évoquant un néant insondable et elles sont sous l'effet d'une peur.
+Description effet : 
+Petits points violets et rouge qui montent doucement puis tête du bâton qui clignote en rouge et violet
+*/
+
+
+
+void visageCauchemar(uint8_t wait){
+    for(uint8_t i=NUM_LEDS ; i>4 ; i=i-2){
+        strip.setPixelColor(i, 255, 0, 255);
+        strip.setPixelColor(i-1, 255, 0, 0);
+        strip.setPixelColor(i+1, 0, 0, 0);
+        strip.setPixelColor(i+2, 0, 0, 0);
+        strip.show();
+        delay(wait);
+    }
+    for(uint8_t i=0 ; i<20 ; i++){                                 //strob ON
+        for(uint8_t j=0 ; j<10 ; j=j+2){
+            strip.setPixelColor(j, 255, 0, 255);
+            strip.setPixelColor(j-1, 255, 0, 0);
+            strip.show();
+        }
+        delay(wait);
+        setAll(0,0,0);                                              //strob OFF
+        delay(wait);
+    }
+}
+
+/*
+Effet 3
+Affichage en led: 000
+Nom du sort : Ténèbre
+Effet du sort :
+Ce sort crée une zone de ténèbres  impénétrable dans toute une pièce. Si vous connaissez ce sort, vous gagnez la capacité passive de voir dans les ténèbres. Si la zone de ténèbres est touchée par une lumière purificatrice, elle est dissipée.
+Ce sort ne fonctionne pas en extérieur.
+Description effet : 
+Allume en violet le pied du bâton
+effet très simple
+*/
+
+
+void tenebre(uint8_t wait){
+    for(uint8_t i=NUM_LEDS ; i>30 ; i--){
+        strip.setPixelColor(i, 255, 0, 255);
+    }
+    strip.show();
+    delay(wait);
+    for(uint8_t i=NUM_LEDS ; i>30 ; i--){
+        strip.setPixelColor(i, 0, 0, 0);
+    }
+    strip.show();
+}
+
+/*
+Effet 4
+Affichage en led: 000
+Nom du sort : Manteau d’ombre
+Effet du sort :
+Vous pouvez devenir invisible, vous drapant dans des ombres magiques.
+Vous ne pouvez pas vous déplacer avec ce sort.
+Vous pouvez le maintenir aussi longtemps que vous le souhaitez.
+Description effet : 
+Les leds brillent comme des étoiles en violet de façon disparate sur le bâton
+→ code quentin?
+*/
+
+
+void manteauDombre(void){
+
+}
+
+/*
+Effet 5
+Affichage en led: 000
+Nom du sort : Amitié morts vivants
+Effet du sort :
+En lançant ce sort sur un mort vivant, celui ci devient votre ami, il ne vous attaquera plus mais ne vous obéira pas forcément.
+Description effet : 
+Rayon vert qui part du bas jusqu’en haut et laisse allumé le haut
+*/
+
+
+void amitieMortsVivants(uint8_t wait){
+ for(uint8_t i=NUM_LEDS ; i>0 ; i--){
+        strip.setPixelColor(i, 0, 255, 0);
+        strip.setPixelColor(i+10, 0, 0, 0);
+        strip.show();
+        delay(wait);
+    }
+    delay(wait*10);
+}
+
+/*
+Effet 6
+Affichage en led: 000
+Nom du sort : Désenchantement
+Effet du sort :
+Vous êtes capable de libérer l'étincelle de vie enfermée dans un objet magique.
+Cela détruit la magie de l'objet.
+Description effet : 
+Faire clignoter très doucement le haut du bâton, s’allume lentement, s’éteint lentement *5
+*/
+
+
+void desenchantement(uint8_t wait){
+for(uint8_t k=0; k<5 ; k++){
+    for(uint8_t j=0; j<255 ; j++){
+        for(uint8_t i=0; i<10; i++) {
+            strip.setPixelColor(i, j, 0, j);
+            strip.show();
+        }
+        delay(wait);
+    }
+    for(uint8_t j=255; j>0 ; j--){
+        for(uint8_t i=0; i<10; i++) {
+            strip.setPixelColor(i, j, 0, j);
+            strip.show();
+        }
+        delay(wait);
+    }
+  }
+}
+
+/*
+Effet 7
+Affichage en led: 000
+Nom du sort : Désorientation
+Effet du sort :
+vous désorientez votre cible qui est alors prise de vertige et doit poser un genou à terre, elle peut ensuite se relever.
+Description effet : 
+Stroboscope violet et blanc haut du bâton
+*/
+
+
+void desorientation(uint8_t wait){
+    for(uint8_t i=0 ; i<20 ; i++){                                 //strob ON
+        for(uint8_t j=0 ; j<10 ; j=j+2){
+            strip.setPixelColor(i, 255, 0, 255);
+            strip.setPixelColor(i-1, 255, 255, 255);
+            strip.show();
+        }
+        delay(wait);
+        setAll(0,0,0);                                          //strobe OFF
+        delay(wait);
+    }
+}
+    
+/*
+Effet 8
+Affichage en led: 000
+Nom du sort : La mort peut attendre
+Effet du sort :
+La cible n'agonisera pas la prochaine fois qu'elle tombera, il restera dans le coma à attendre. Le sort reste actif jusqu'à utilisation ou jusqu'au prochain repas. Le magicien peut lancer autant de fois ce sort qu'il le souhaite sur plusieurs personnes à la fois.
+Description effet : 
+lueure blanche en haut du bâton?
+
+*/
+
+
+void laMortPeutAttendre(uint8_t wait){
+    delay(wait);
+}
+
 /**************************** FONCTION PARAMETRES **********************/
 
 void showStrip() {
@@ -304,7 +499,7 @@ void setAll(byte red, byte green, byte blue) {
     setPixel(i, red, green, blue); 
   }
   showStrip();
-}
+}	
 
 bool startBreak(){
   bool newStart = digitalRead(BUTTON_PIN_START);
